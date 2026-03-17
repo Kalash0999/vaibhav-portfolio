14a21c4759625a648e68fb901006eef02e759a2c
Vaibhav Tiwari <KALASHTIWARI85@GMAIL.COM>
2026-03-17 17:13:25 +0530

    fix: vite config for vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'],
+
+  optimizeDeps: {
+    include: [
+      'three',
+      '@react-three/fiber',
+      '@react-three/drei'
+    ]
   },
+
   build: {
-    chunkSizeWarningLimit: 800,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules/three')) {
-            return 'three-core'
-          }
-
-          if (
-            id.includes('@react-three/fiber') ||
-            id.includes('@react-three/drei')
-          ) {
-            return 'three-react'
-          }
-
-          if (id.includes('node_modules/gsap')) {
-            return 'gsap'
-          }
-        },
-      },
-    },
-  },
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 })
